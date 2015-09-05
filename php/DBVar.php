c9640caa01d50c1f6581f180e944db8412ab8781
buirai <buirai_naetle@hotmail.com>
2015-09-05 17:30:35 -0500

    Create student

---
+++ php/DBVar.php
@@ -0,0 +1,6 @@
+<?php 
+	$DBName = "ajaxt1";
+	$DBUser = "root";
+	$DBPass = "";
+	$DBServer = "localhost";
+?>
---
+++ php/saveStudent.php
@@ -0,0 +1,32 @@
+<?php 
+	
+	header("Content-Type:text/plain");
+
+	$id = $_POST["id"];
+	$name = $_POST["name"];
+	$lastname = $_POST["lastname"];
+	$birthdate = $_POST["birthdate"];
+	$dateAdmission = $_POST["dateAdmission"];
+	$gender = $_POST["gender"];
+	$email = $_POST["email"];
+	$career = $_POST["career"];
+
+	include "DBVar.php";
+
+	$link = mysqli_connect($DBServer, $DBUser, $DBPass, $DBName);
+	
+	$query = "INSERT INTO `ajaxt1`.`student` (`id`, `name`, `lastname`, `birthdate`, `dateAdmission`, `gender`, `email`, `career`) VALUES ('$id', '$name', '$lastname', '$birthdate', '$dateAdmission', '$gender', '$email', '$career');";
+	
+	if (!$link) {
+		die("<h3 class='text-danger'>Error de Conexión (" . mysqli_connect_errno() . ") ". mysqli_connect_error() . "</h3>");
+	}
+
+	if (mysqli_query($link, $query) == true) {
+		echo "<h3 class='text-success'>Se ha creado con éxito el estudiante</h3>";
+	}else{
+		echo "<h3 class='text-danger'>NO se creó el estudiante, algo ocurrio</h3>";
+	}
+
+	mysqli_close($link);
+
+?>
