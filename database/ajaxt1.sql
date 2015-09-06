4ec136b81794b63565be28c5e88f55d2cb9e43fc
buirai <buirai_naetle@hotmail.com>
2015-09-06 13:42:07 -0500

    ajaxt1.sql modified

--- database/ajaxt1.sql
+++ database/ajaxt1.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 06-09-2015 a las 01:48:40
+-- Tiempo de generación: 06-09-2015 a las 20:41:15
 -- Versión del servidor: 5.6.26
 -- Versión de PHP: 5.6.12
 
@@ -42,7 +42,32 @@ CREATE TABLE IF NOT EXISTS `student` (
 --
 
 INSERT INTO `student` (`id`, `name`, `lastname`, `birthdate`, `dateAdmission`, `gender`, `email`, `career`) VALUES
-('11', 'sakari02', 'asd', 'asd', 'asd', 'F', 'buirai_naetle@hotmail.com', 'qqqqq');
+('22222222', 'Maria', 'Arvizu Olivas', '02-02-1890', '02-02-2015', 'F', 'marvizu@gmail.com', 'Actriz de doblaje'),
+('33333333', 'Felicia', 'Arvizu Olivas', '30-01-1890', '10-02-2014', 'F', 'fel@gmail.com', 'LIS'),
+('99999999', 'Jorge', 'Lopez Duran', '10-10-2000', '10-10-2010', 'M', 'jorge@gmail.com', 'LCC');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tutor`
+--
+
+CREATE TABLE IF NOT EXISTS `tutor` (
+  `id` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `lastname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `gender` varchar(1) COLLATE utf8_unicode_ci NOT NULL,
+  `area` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `tutor`
+--
+
+INSERT INTO `tutor` (`id`, `name`, `lastname`, `gender`, `area`, `email`) VALUES
+('12121212', 'nadie', 'nunca jamas ', 'I', 'MAT', 'vacio@nada.com'),
+('66666666', 'Juan Pablo', 'Ucan Pech', 'M', 'LIS', 'juan.ucan@uady.mx');
 
 --
 -- Índices para tablas volcadas
@@ -54,10 +79,12 @@ INSERT INTO `student` (`id`, `name`, `lastname`, `birthdate`, `dateAdmission`, `
 ALTER TABLE `student`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indices de la tabla `tutor`
+--
+ALTER TABLE `tutor`
+  ADD PRIMARY KEY (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-INSERT INTO `ajaxt1`.`student` (`id`, `name`, `lastname`, `birthdate`, `dateAdmission`, `gender`, `email`, `career`) VALUES 
-('11111111', 'Jose', 'Santos Bolaños', '01-01-1993', '01-01-2015', 'M', 'jose@gmail.com', 'Ing. de Software'), 
-('22222222', 'Maria', 'Arvizu Olivas', '02-02-1890', '02-02-2015', 'F', 'marvizu@gmail.com', 'Actriz de doblaje');
