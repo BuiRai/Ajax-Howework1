9dd2d58a0b158d904e188d0c8ce3b760856afd43
buirai <buirai_naetle@hotmail.com>
2015-09-07 22:34:37 -0500

    I have finished the login

--- index.php
+++ index.php
@@ -1,35 +1,57 @@
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Indice</title>
 	<link rel="stylesheet" href="css/bootstrap.css">
-	<script src="javascript/zxml.js"></script>
-	<script src="javascript/manager/getManagerData.js"></script>
 </head>
 <body>
 	<div class="container">
-		<h1 class="text-center">Ingresa tus datos</h1>
-		<div class="form-horizontal">
-		  <div class="form-group">
-		    <label class="control-label col-sm-2" for="username">Nombre de usuario:</label>
-		    <div class="col-sm-10">
-		      <input type="text" class="form-control" id="managerUsername" name="username" placeholder="Escriba su nombre de usuario">
+		<?php 
+		if (!$_SESSION) {
+		?>
+			<h5 class="text-center text-muted"><dt>Para ingresar utilice:</dt></h5>
+			<h5 class="text-center text-muted"><dt>nombre:</dt> admin</h5>
+			<h5 class="text-center text-muted"><dt>password:</dt> admin</h5>
+			<h1 class="text-center">Ingresa tus datos</h1>
+			<form id="formLogin" class="form-horizontal" method="post" action="php/manager/getManagerData.php">
+			  <div class="form-group">
+			    <label class="control-label col-sm-2" for="username">Nombre de usuario:</label>
+			    <div class="col-sm-10">
+			      <input type="text" class="form-control" id="managerUsername" name="username" placeholder="Escriba su nombre de usuario" required>
+			    </div>
+			  </div>
+			  <div class="form-group">
+			    <label class="control-label col-sm-2" for="pass">Contraseña:</label>
+			    <div class="col-sm-10"> 
+			      <input type="password" class="form-control" id="managerPass" name="pass" placeholder="Ingrese su contraseña" required>
+			    </div>
+			  </div>
+			  <div class="form-group"> 
+			    <div class="col-sm-offset-2 col-sm-10">
+			      <button type="submit" class="btn btn-success">Ingresar</button>
+			    </div>
+			  </div>
+			</form>
+		<?php 
+		}else{
+		?>
+			<div class="panel panel-primary">
+		      <div class="panel-heading">Panel with panel-primary class</div>
+		      <div class="panel-body">
+		      	<div class="list-group">
+				  <a href="views/student/create.html" class="list-group-item">Administrar estudiantes</a>
+				  <a href="views/tutor/create.html" class="list-group-item">Administrar tutores</a>
+				</div>
+		      </div>
 		    </div>
-		  </div>
-		  <div class="form-group">
-		    <label class="control-label col-sm-2" for="pass">Contraseña:</label>
-		    <div class="col-sm-10"> 
-		      <input type="password" class="form-control" id="managerPass" name="pass" placeholder="Ingrese su contraseña">
-		    </div>
-		  </div>
-		  <div class="form-group"> 
-		    <div class="col-sm-offset-2 col-sm-10">
-		      <button type="submit" class="btn btn-primary" onclick="requestCustomerInfo()">Ingresar</button>
-		    </div>
-		  </div>
-		</div>
-		<div id="divManagerInfo" class="text-center text-info"></div>
+		    <a href="php/manager/logout.php">Cerrar sesión</a>
+		<?php
+		}
+		?>
 	</div>
 </body>
 </html>
--- php/manager/getManagerData.php
+++ php/manager/getManagerData.php
@@ -1,9 +1,9 @@
 <?php 
 	header("Content-Type:text/plain");
-	$name = $_GET["username"];
-	$pass = $_GET["pass"];
+	$name = $_POST["username"];
+	$pass = $_POST["pass"];
 	include "../DBVar.php";
-	
+	echo "dasdas";
 	$link = mysqli_connect($DBServer, $DBUser, $DBPass, $DBName);
 
 	$query = "SELECT * FROM `manager` WHERE name = '$name'";
@@ -17,17 +17,14 @@
 
 	if ( $name == $row[1] && $pass == $row[2] ) {
 		$row = mysqli_fetch_array($result);
-		echo "<h2 class='text-success'>Bienvenido $name, administre:</h2>";
-
-?>
-		<div class="list-group">
-		  <a href="views/student/create.html" class="list-group-item">Administrar estudiantes</a>
-		  <a href="views/tutor/create.html" class="list-group-item">Administrar tutores</a>
-		</div>
-<?php
+		session_start();
+		$_SESSION["name"] = $name;
+		$_SESSION["pass"] = $pass;
+		
+		header("location:../../");
 	}else{
-		echo "<h2 class='text-danger'>No se encuentra registrado este usuario, intente de nuevo.</h2>";
+		header("location:../../");
 	}
-
 	mysqli_close($link);
+
 ?>
